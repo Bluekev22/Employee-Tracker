a186e52ea1476336bbfa2cefb1594f60e8dbb9b6
Kevin Shank <kevin_shank@yahoo.com>
2021-08-24 11:10:52 -0400

    added db connection

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
+const express = require('express');
 const inquirer = require("inquirer");
+const cTable = require('console.table');
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      
+      user: 'root',
+      
+      password: 'password',
+      database: 'courses_db'
+    },
+    console.log(`Connected to the courses_db database.`)
+  );
+  
+
+
+  // Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });

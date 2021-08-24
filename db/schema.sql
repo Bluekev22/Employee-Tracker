c6feb8962bef2197751d838539fa19f4922caf15
Kevin Shank <kevin_shank@yahoo.com>
2021-08-24 13:37:27 -0400

    added tables to database

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS staffing_db;
+CREATE DATABASE staffing_db;
+
+USE staffing_db;
+
+CREATE TABLE department (
+  id INT NOT NULL PRIMARY KEY,
+  department_name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT NOT NULL
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT
+    FOREIGN KEY (role_id)
+    REFERENCES roles(id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+);
